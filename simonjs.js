44634c858670aa07fec6bbe118c24e3062055836
Kyle Stengline <kyle.stengline@gmail.com>
2015-12-16 16:08:43 -0500

    Adds Simon Game with jQuery

---
+++ simonjs.js
@@ -0,0 +1,20 @@
+"use strict";
+
+$(function() {
+ console.log("loaded");
+
+
+var simonColor = ["blue", "red", "green", "yellow"];
+
+var colors = $("div").attr("class", "block");
+
+
+var addEventListeners = function() {
+
+
+
+}
+
+
+
+})();
