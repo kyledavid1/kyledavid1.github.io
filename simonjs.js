da8e5e6baeaa6002d38d22a818a5c6848528443a
Kyle Stengline <kyle.stengline@gmail.com>
2015-12-17 11:32:18 -0500

    Update to Simon 121715 1132

--- simonjs.js
+++ simonjs.js
@@ -6,11 +6,12 @@ $(document).ready(function() {
 
 var simonColor = ["blue", "red", "green", "yellow"];
 
-// ar simonText = $("#black").tevxt("Simon").css("color", "white");
-// simonText.css("font-size", "50pt");
-// simonText.css("text-align: center");
+var simonText = $("#black").text("Simon").css("color", "white");
+simonText.css("font-size", "50pt");
 
 var addEventListeners = function() {
+
+
 
 
 
