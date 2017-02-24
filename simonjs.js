c53cf8ec2a9c3fc473d9dde144df81f5c926813c
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-24 14:23:35 -0800

    refactors code and fixes player wins function

--- simonjs.js
+++ simonjs.js
@@ -19,7 +19,8 @@ $(document).ready(function() {
     simonText.text("Simon");
   });
 
-	// newGame starts the sequence. I'm utilizing a for loop to push a random number from the simonColor variable into the empty sequence array. The reason for generating a random number is because simonColor is an array with the values 0 - 3. These values correlate directly with the 4 colors in simonColor. Math.round is rounding the number to a whole number because Math.random generates a number between 0 and 1 (0.344) which is why I multiply it by simonColor as simonColor has 4 colors in it. However, arrays start counting at 0, whcih means, in array standards, simonColor has 0, 1, 2, 3 values(colors) within it and that is why I subtract 1 from simonColor.length. Then I call playSequence, so the computer plays the randomly generated sequence to the player.
+  /* newGame starts the sequence.
+  Then I call playSequence, so the computer plays the randomly generated sequence to the player.*/
 	var newGame = function() {
 		sequence = [];
 		var gameOver = false;
@@ -29,7 +30,8 @@ $(document).ready(function() {
     playSequence();
 	};
 
-	// This plays the sequence and advances to the next level when the proper sequence has been selected. In simonColor.forEach The '#'' is calling the id of the boxes +the color and turning the click off for the duration the sequence is being played. The sequence.forEach is taking in the color and the index of that color(0, 1, 2..etc.) and playing that sequence for a duration of 700, miliseconds. In other words, it's lighting up the color for that time. The second timeout function is taking the index and * it by 1 second. I do this because as the sequence gets longer, I'm giving it more time to play the sequence out and making sure the user is unable to click while the sequence is being played. activateButtons is then allowing the user to click the sequence and is also giving the user enough time to click the proper sequence.
+	/*This plays the sequence and advances to the 
+  next level when the proper sequence has been selected.*/
 	var playSequence = function() {
 		simonColor.forEach(function(color) {
 			$("#" +color).off("click");
@@ -46,14 +48,17 @@ $(document).ready(function() {
 		setTimeout(activateButtons, level * 1000)
 	};
 
-	// Activating the click function so the user can then click, but only after the sequence has been played in full.
+	// Activating the click function
+  // user clicks after sequence plays
 	var activateButtons = function() {
 		simonColor.forEach(function(color) {
 			$("#" +color).on("click", clickHandler);
 		});
 	};
 
-	// This is allowing the click functionality for the game. Once clicked the colors will light up for half a second. If the correct sequence is clicked you will advance to the next level via the level++ and the newGame function allows an entirely new sequence to be played as well. The if statement is also checking to make sure the user has clicked everything in that sequence and if they have then advance to the next level. If not the game will wait until the user has clicked everything in the sequence. Sequence.shift is shifting the first color off, comparing it to the color just clicked then checking for the next color clicked. 
+	/* Enabling click functionality. 
+   lights buttons when clicked 
+   checks proper sequence is clicked*/
 	var clickHandler = function() {
 		var color = this
 		$(this).toggleClass("light");
@@ -67,30 +72,27 @@ $(document).ready(function() {
 			if (sequence.length === 0) {
         setTimeout(function() {
           simonText.text("Level "+level);
-          level++
+          level++;
           newGame();
         }, 2000)
       }
     } else {
       $("#").off("click");
+      simonText.css("font-size", "40px");
       simonText.text("You Lost! Restart");
       wrongBeep.play();
       level = 1;
-      newGame();
     }	
-			playerWins();
+  playerWins();
 	};	
 		
-		// This is stating that the player will win if they reach level 10 and will display a message within the black box. However, this function will keep playing after the user has reached level 10. I need to make the game display the message and stop playing the sequence after that level has been reached. 
+  //player wins after level 10 completion.
   var playerWins = function() {
-    if (level > 10) {
-      if (sequence.length === 0) {
-        alert("CONGRATS! YOU WON");
-        simonText.text("YOU WON!");
-        $("#").off("click");
-          level = 1;
-        newGame();
-      }
-    }	
-  }
+    if (level > 10 && sequence.length === 0) {
+      simonText.text("YOU WON!");
+      setTimeout(function(){
+        location.reload();
+      }, 1900);
+    }
+  }	
 });
